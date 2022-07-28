1935fa071e908ea1089d55159fd02d3c3ebadc7e
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-28 19:42:52 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import re
+
+output=''
+
+com=requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt').text
+com=re.sub('#.*\n','',com).split('\n')
+com.remove('')
+for one in com:
+  output+='server=/%s/127.0.0.1#5335\n'%one
+with open('docs/dnsmasq-chinadns.conf','w') as f:
+  f.write(output)
